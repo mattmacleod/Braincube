f9a9a697f9dd7f0f74c87d0dd6d32934d51366a1
Matthew MacLeod <matt@matt-m.co.uk>
2011-05-16 08:01:04 +0100

    Updated config

--- config/braincube.rb
+++ config/braincube.rb
@@ -2,41 +2,40 @@
 
 class Braincube::Config
   
+  
   # Site configuration
+  ############################################################################
   SiteTitle   = "Braincube"
-  SiteEmail   = "hello@braincube.co.uk"
-  SiteBaseUrl = "http://www.braincube.co.uk"
+  SiteEmail   = "hello@braincu.be"
+  SiteBaseUrl = "http://www.braincu.be"
+  
   
   # Access and authentication
+  ############################################################################
   SessionTimeout = 4.hours
   AdminRoles     = ["ADMIN", "PUBLISHER","SUBEDITOR", "EDITOR", "WRITER"]
   
-  # Validation Regexps
+  
+  # Validation
+  ############################################################################
   EmailRegexp =  /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
   UrlRegexp   =  /[a-zA-Z0-9_]+/i
   
-  # Article types
+  
+  # Article  and page types
+  ############################################################################
   ArticleTemplates  = ["Normal"]  
   ArticleTypes  = { 
-    :article       => "Generic article",
-    :album_review  => "Album review",
-    :single_review => "Single review",
-    :gig_review    => "Gig review",
-    :film_review   => "Film review",
-    :event_preview => "Event preview",
-    :book_review   => "Book review",
-    :game_review   => "Game review",
-    :venue_review  => "Venue review",
+    :article       => "Article",
     :feature       => "Feature",
-    :review        => "Generic review",
-    :preview       => "Generic preview",
+    :review        => "Review",
     :blog_post     => "Blog post",
     :gallery       => "Gallery",
     :video         => "Video"
   } 
   PageTypes = {
     :home        => "Home page",
-    :section     => "Section landing page",
+    :section     => "Landing page",
     :articles    => "Article listing",
     :events      => "Event listing",
     :venues      => "Venue listing",
@@ -44,7 +43,6 @@ class Braincube::Config
     :blogs       => "Blog listing",
     :staff       => "Staff listing",
     :text        => "Static text",
-    :competition => "Competition",
     :contact     => "Contact form"
   }
   PageSortOrder = {
@@ -52,8 +50,11 @@ class Braincube::Config
     :name   => "Alphabetical"
   }
   
+  
+  # Image file types
   # Magick geometry string. Always leave :thumb in this list or bad things will
   # happen elsewhere. You've been warned.
+  ############################################################################
   ImageFileVersions = { 
     :large   => ["900x675>",   :jpg],
     :wide    => ["540x",      :jpg],
@@ -62,7 +63,9 @@ class Braincube::Config
     :tiny    => ["60x40#",    :jpg]
   }
   
+  
   # Asset uploading
+  ############################################################################
   AssetMaxUploadSize = 20.megabytes
   AssetContentTypes = [
     "image/jpeg", "image/pjpeg", "image/png", "image/x-png", "image/gif",
@@ -70,16 +73,24 @@ class Braincube::Config
     "application/zip"
   ]
   
-  # Admin menu system
+  
+  # Admin menus
+  ############################################################################
   AdminMenus = YAML::load( File.read( File.dirname(__FILE__) + '/braincube_menu.yml' ) )
   
+  
   # API keys
+  ############################################################################
   GoogleApiKey = "ABQIAAAA_a2Y8YnvGdeGgoGFUyYotBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQDT74lJaTBe_PAvumnRp3_dubVrg"
   
+  
   # Mapping
+  ############################################################################
   DefaultAdminMapLocation = "53.800651,-4.042969"
   
-  # Affiliates
+  
+  # Event options
+  ############################################################################
   AffiliateCodes = {
     "Ticket Master" => "ticketmaster"
   }
@@ -91,7 +102,9 @@ class Braincube::Config
     "Advance purchase" => "advance"
   }
   
-  # Admin counts
+  
+  # Limits for UI in admin
+  ############################################################################
   AdminPaginationLimit = 50
   AdminAssetPaginationLimit = 20
   EventAttachmentLimit = 20
@@ -99,26 +112,18 @@ class Braincube::Config
   
   
   # Widgets
+  ############################################################################
   WidgetTypes = {
-    :ad_slot         => "Advert slot",
     :latest_tagged   => "Latest tagged articles",
     :latest_comments => "Latest comments",
     :custom_html     => "Custom HTML",
     :image           => "Image link"
   }
 
-  WidgetAdSlots = {
-    1 => "Leaderboard",
-    2 => "Top MPU",
-    3 => "Skyscraper",
-    4 => "Banner",
-    5 => "Bottom MPU"
-  }
-  
   WidgetSlots = {
     :header         => "Header bar",
-    :sidebar        => "Sidebar",
-    :skyscraper_bar => "Skyscraper bar"
+    :sidebar_1      => "Sidebar 1",
+    :sidebar_2      => "Sidebar 2"
   }
   
 end
