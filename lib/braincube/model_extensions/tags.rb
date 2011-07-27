2694970fae5962c0b9179a3c29ba991beff30996
Matthew MacLeod <matt@matt-m.co.uk>
2011-07-27 14:04:52 +0100

    Another tag fix

--- lib/braincube/model_extensions/tags.rb
+++ lib/braincube/model_extensions/tags.rb
@@ -105,7 +105,7 @@ module Braincube #:nodoc:
           tag_ids = self.taggings.map(&:tag_id)
           return where("1=0") if tag_ids.blank?
           
-          taggable_ids = Tagging.select("taggings.taggable_id, COUNT(tag_id) AS tag_count").where("taggable_id!=#{id}").where(:tag_id => tag_ids).where(:taggable_type => self.class.name).group("taggable_id, taggable_type").order("COUNT(taggable_id) DESC").limit(count).map(&:taggable_id)
+          taggable_ids = Tagging.select("taggings.taggable_id, COUNT(tag_id) AS tag_count").where("taggable_id!=#{id}").where("tag_count >= 2").where(:tag_id => tag_ids).where(:taggable_type => self.class.name).group("taggable_id, taggable_type").limit(count).map(&:taggable_id)
           return where("1=0") if taggable_ids.blank?
           
           return self.class.where(:id => taggable_ids)
