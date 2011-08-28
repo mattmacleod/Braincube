6d523795e98f98f5e2151a87c71cd4670d53497f
Matthew MacLeod <matt@matt-m.co.uk>
2011-08-28 14:20:31 +0100

    Fixed validation on asset folders

--- app/models/asset_folder.rb
+++ app/models/asset_folder.rb
@@ -12,7 +12,7 @@ class AssetFolder < ActiveRecord::Base
   
   # Validations
   validates :name, :presence => true
-  validates_uniqueness_of :name, :scope => :parent_id
+  validates_uniqueness_of :name, :scope => :parent_id, :case_sensitive => false
   validates_associated :parent
   validates :parent_id, :tree => true
   
