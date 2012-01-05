49c7651191919b8487f254e253b029c5e1bf28b0
Matthew MacLeod <matt@matt-m.co.uk>
2012-01-05 13:06:52 +0000

    Widget fix

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -9,7 +9,7 @@ class Widget < ActiveRecord::Base
   
   # Validations
   validates_presence_of :title, :widget_type
-  attr_accessible :title, :widget_type
+  attr_accessible :title, :widget_type, :properties
   
   # Library bits
   braincube_has_properties
