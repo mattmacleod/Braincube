58089affd8994e85318d6508977c6ccfc3a1d36b
Matthew MacLeod <matt@matt-m.co.uk>
2012-03-14 18:17:18 +0000

    Explicitly allowed script src tags

--- public/javascripts/admin/tinymce.js
+++ public/javascripts/admin/tinymce.js
@@ -27,7 +27,7 @@ braincube.admin.tinymce = {
 					relative_urls : false,
 					
 					content_css : "/stylesheets/editor_content.css",
-					valid_elements : "*[*]",
+					valid_elements : "*[*],script[type|language|src]",
 					
 					init_instance_callback: 'braincube.admin.tinymce.word_counter.init',
 					
